db09df666a1655a0a245bc58ca2b18701057be3f
denisehodgson <denisehodgson27@gmail.com>
2022-09-16 18:32:49 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,8 +1,8 @@
- var rotationSpeed = 0.01;
+ var rotationSpeed = 0.07;
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y -= rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
